cf23890fd456769f54e7fb1f302b5c40cf47be14
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-01-06 19:43:38 +0000

    Minor fix.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,6 +1,9 @@
 const ListItem = ({song, index}) => {
     return (
+        <>
         <li>Position: {index + 1} - Artist: {song["im:artist"].label} - Title: {song["im:name"].label}</li>
+        <br></br>
+        </>
     )
         
     
